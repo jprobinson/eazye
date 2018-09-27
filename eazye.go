c5f4e1a978f5e9cf2785df6a02062fe92f97c949
JP Robinson <john.paul.robinson@gmail.com>
2018-09-27 16:55:32 -0400

    using stdlib to parse encoded subejcts

--- eazye.go
+++ eazye.go
@@ -11,7 +11,6 @@ import (
 	"mime"
 	"mime/multipart"
 	"net/mail"
-	"strconv"
 	"strings"
 	"time"
 
@@ -396,95 +395,14 @@ func isEncodedWord(word string) bool {
 	return strings.HasPrefix(word, "=?") && strings.HasSuffix(word, "?=") && strings.Count(word, "?") == 4
 }
 
-// decodeRFC2047Word was blatantly ripped off from the net/mail package.
-// Hopefully https://code.google.com/p/go/issues/detail?id=4687 will be in 1.4 and we wont need this!
-func decodeRFC2047Word(s string) (string, error) {
-	fields := strings.Split(s, "?")
-	if len(fields) != 5 || fields[0] != "=" || fields[4] != "=" {
-		return "", errors.New("address not RFC 2047 encoded")
-	}
-	charset, enc := strings.ToLower(fields[1]), strings.ToLower(fields[2])
-	if charset != "iso-8859-1" && charset != "utf-8" {
-		return "", fmt.Errorf("charset not supported: %q", charset)
-	}
-
-	in := bytes.NewBufferString(fields[3])
-	var r io.Reader
-	switch enc {
-	case "b":
-		r = base64.NewDecoder(base64.StdEncoding, in)
-	case "q":
-		r = qDecoder{r: in}
-	default:
-		return "", fmt.Errorf("RFC 2047 encoding not supported: %q", enc)
-	}
-
-	dec, err := ioutil.ReadAll(r)
-	if err != nil {
-		return "", err
-	}
-
-	switch charset {
-	case "iso-8859-1":
-		b := new(bytes.Buffer)
-		for _, c := range dec {
-			b.WriteRune(rune(c))
-		}
-		return b.String(), nil
-	case "utf-8":
-		return string(dec), nil
-	default:
-		return s, fmt.Errorf("charset unknown: %s", charset)
-	}
-}
-
-type qDecoder struct {
-	r       io.Reader
-	scratch [2]byte
-}
-
-func (qd qDecoder) Read(p []byte) (n int, err error) {
-	// This method writes at most one byte into p.
-	if len(p) == 0 {
-		return 0, nil
-	}
-	if _, err := qd.r.Read(qd.scratch[:1]); err != nil {
-		return 0, err
-	}
-	switch c := qd.scratch[0]; {
-	case c == '=':
-		if _, err := io.ReadFull(qd.r, qd.scratch[:2]); err != nil {
-			return 0, err
-		}
-		x, err := strconv.ParseInt(string(qd.scratch[:2]), 16, 64)
-		if err != nil {
-			return 0, fmt.Errorf("mail: invalid RFC 2047 encoding: %q", qd.scratch[:2])
-		}
-		p[0] = byte(x)
-	case c == '_':
-		p[0] = ' '
-	default:
-		p[0] = c
-	}
-	return 1, nil
-}
-
 func parseSubject(subject string) string {
 	if !hasEncoding(subject) {
 		return subject
 	}
 
-	var words []string
-	subs := strings.SplitN(subject, " ", -1)
-	for _, word := range subs {
-		if isEncodedWord(word) {
-			// we dont care if it bombed. just take the word
-			word, _ = decodeRFC2047Word(word)
-		}
-		words = append(words, word)
-	}
-
-	return strings.Join(words, " ")
+	dec := mime.WordDecoder{}
+	sub, _ := dec.DecodeHeader(subject)
+	return sub
 }
 
 // NewEmail will parse an imap.FieldMap into an Email. This
